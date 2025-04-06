9a43928c2441e64a7d08193f98ebe69112a66f93
Tornike <bakradze1203@gmail.com>
2025-04-06 18:02:13 +0400

    first commit

---
+++ src/test/java/ge/ecomerce/taskteamtracker/TaskTeamTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.ecomerce.taskteamtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTeamTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
